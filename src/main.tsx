85eeb2b15927b38612f2f0a168967ef0aa5e01db
sivan22 <sivan.raston@gmail.com>
2026-05-13 19:54:22 +0000

    chore: drop runHistory module and clean up old localStorage key

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+try { localStorage.removeItem('llm_ocr_web:runHistory:v1'); } catch { /* ignore */ }
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
